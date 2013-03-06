2649e1224a0d8b0a3113d7533e72b1f6952ae95c
flynsarmy <flynsarmy@gmail.com>
2013-03-07 09:57:10 +1000

    first commit

---
+++ classes/flynsarmywysiwygimages_module.php
@@ -0,0 +1,90 @@
+<?
+
+	class FlynsarmyWYSIWYGImages_Module extends Core_ModuleBase {
+		protected function createModuleInfo() {
+			return new Core_ModuleInfo(
+				"WYSIWYG Images",
+				"Adds improved image editing functionality to WYSIWYG fields.",
+				"Flynsarmy"
+			);
+		}
+
+		public function subscribeEvents() {
+			Backend::$events->addEvent('blog:onExtendPostForm', $this, 'extend_blog_post_form');
+			Backend::$events->addEvent('shop:onExtendProductForm', $this, 'extend_shop_product_form');
+			Backend::$events->addEvent('cms:onExtendPageForm', $this, 'extend_cms_page_form');
+			Backend::$events->addEvent('shop:onExtendCategoryForm', $this, 'extend_shop_category_form');
+			Backend::$events->addEvent('flynsarmyprodfields:afterFieldAdded', $this, 'extend_custom_prod_field');
+			Backend::$events->addEvent('flynsarmyslideshow:onExtendSlideForm', $this, 'extend_slideshow_slide_form');
+		}
+
+		/**
+		 * Blog post extensions
+		 */
+
+		public function extend_blog_post_form($post, $context) {
+			if ( $context != 'preview' )
+			{
+				$field = $post->find_form_field('content');
+				$field->htmlPlugins .= ',advimage';
+				$field->htmlButtons1 .= ',advimage';
+			}
+		}
+
+		/**
+		 * Shop product extensions
+		 */
+
+		public function extend_shop_product_form($product, $context) {
+			if ( !in_array($context, array('preview', 'option-matrix')) )
+			{
+				$field = $product->find_form_field('description');
+				$field->htmlPlugins .= ',advimage';
+				$field->htmlButtons1 .= ',advimage';
+			}
+		}
+
+		public function extend_custom_prod_field($field_details, $field)
+		{
+			if ( $field_details->type == FlynsarmyProdFields_Field::FIELD_WYSIWYG )
+			{
+				$field->htmlPlugins .= ',advimage';
+				$field->htmlButtons1 .= ',advimage';
+			}
+		}
+
+		/**
+		 * Shop category extensions
+		 */
+
+		public function extend_shop_category_form($category) {
+			$field = $category->find_form_field('description');
+			$field->htmlPlugins .= ',advimage';
+			$field->htmlButtons1 .= ',advimage';
+		}
+
+		/**
+		 * CMS page extensions
+		 */
+
+		public function extend_cms_page_form($page) {
+			$blocks = $page->list_content_blocks();
+
+			foreach($blocks as $block) {
+				$column_name = 'content_block_' . $block->code;
+
+				$field = $page->find_form_field($column_name);
+				$field->htmlPlugins .= ',advimage';
+				$field->htmlButtons1 .= ',advimage';
+			}
+		}
+
+		/**
+		 * Slideshow slide extensions
+		 */
+		public function extend_slideshow_slide_form($slide, $context) {
+			$field = $slide->find_form_field('description');
+			$field->htmlPlugins .= ',advimage';
+			$field->htmlButtons1 .= ',advimage';
+		}
+	}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# ls-module-wysiwyg-images
+Adds improved image editing functionality to WYSIWYG fields.
+
+## Installation
+1. Download [WYSIWYG Images](https://github.com/flynsarmy/ls-module-wysiwyg-images/zipball/master)
+1. Create a folder named `flynsarmywysiwygimages` in the `modules` directory.
+1. Extract all files into the `modules/flynsarmywysiwygimages` directory (`modules/flynsarmywysiwygimages/readme.md` should exist).
+1. Go to the `Blog Post` or `CMS Page` area and click the image button.
+1. Done!
